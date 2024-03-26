33e63a8a7efc2807dd17268a3559522e0f55c2b3
ricardoggz <ricardo_ggzicm@outlook.com>
2024-03-26 14:27:58 -0600

    completo hasta el 13 de abril

--- index.js
+++ index.js
@@ -10,12 +10,147 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Datos de ejemplo de cumpleañeros
     const birthdays = {
-        2: { // Febrero
-            14: ["Ana", "Juan"], // Cumpleaños el 14 de febrero
-            25: ["María"] // Cumpleaños el 25 de febrero
-        },
-        3: { // Marzo
-            5: ["Pedro"] // Cumpleaños el 5 de marzo
+        3: { // Abril
+            1: [
+                "MORALES ROMAN MARIBEL",
+                "URIBE LUGO MARIA FERNANDA",
+                "DELGADO GARCIA JACQUELINE STEPHANY",
+                "SUZAN BLANCAS EDUARDO",
+            ],
+            2: [
+                "DEL MORAL ESPINOSA IRMA ESTHER",
+                "ZALLES VIDAL CRISTIAN RUBEN",
+                "PALACIOS OVIEDO ROBERTO",
+                "GONZALEZ FLORES OFELIA",
+                "ZUÑIGA LARA ELSA MARIANA",
+                "MARTINEZ CONTRERAS JUANA",
+                "VELA GONZALEZ JUAN CARLOS"
+            ],
+            3: [
+                "GONZALEZ MENDEZ RICARDO",
+                "ORDORICA FLORES RICARDO MANUEL",
+                "MORA LOPEZ MATILDE",
+                "FUERTE MEJIA MICHAEL",
+                "LEAL LARIS GRACIELA",
+                "IBARRA CRUZ RICARDO ROBERTO",
+                "CHAVEZ VEGA SANDRA ANGELINA",
+                "CARRANZA AGUILAR ROSA MARIA"
+            ],
+            4: [
+                "CASTAÑEDA PEÑA PATRICIA IVONNE",
+                "GARCIA ESPINO DENISE",
+                "HERNANDEZ SANCHEZ JORGE",
+                "ROMERO MENDOZA ISIDORO NESTOR",
+                "BUSTOS SALAZAR BARBARA",
+                "LOPEZ ARELLANO ANA KARINA",
+                "JUAREZ DANIEL JOSE LUIS",
+                "SANCHEZ URBINA ROCIO",
+                "RUIZ REYES IRVING ALEJANDRO",
+                "VEGA RAMIREZ MARIA DE LOURDES",
+                "ROMERO BALBUENA VIOLETA PAULINA"
+            ],
+            5: [
+                "MUÑOZ PEÑA MIRIAM",
+                "SANTILLAN ORGAS MARIA ESTHER",
+                "HERNANDEZ TAPIA ANGELICA MARIA",
+                "NAVARRETE BARBOSA EDGAR ISRAEL",
+                "MENDOZA MORALES SANDRA ANGELICA",
+                "GUTIERREZ RUBIO GABRIELA",
+                "PEREZ GARCIA GUADALUPE",
+                "ARELLANO GARCIA KARINA JOSELYN",
+                "CABALLERO GARCIA EGLEN",
+                "PEREZ DOMINGUEZ BRYAN RAFAEL",
+            ],
+            6: [
+                "MOLINA CASTRO GRECIA",
+                "WORONA LILIANA BEATRIZ",
+                "CORTES BENAVIDES MARIA CRISTINA",
+                "REYES FONSECA ANA LAURA",
+                "CANALES ROBREDO CARLOS",
+                "PUGA BUCIO BENJAMIN",
+                "GARCIA SOLIS DIANA JANET",
+                "RIVAS ESQUIVEL ANDREA ITZEL",
+                "BAEZ AVILA IRMA LETICIA",
+                "LOZADA LOPEZ VICTOR ALFONSO",
+            ],
+            7: [
+                "MOYAO GARCIA DIANA",
+                "GUTIERREZ RIVERO MARGARITA",
+                "VEGA TRUJILLO VERONICA",
+                "BAUTISTA LIMON DULCE ESPERANZA",
+                "SAUCEDO TOLEDO MAYRA ELVIRA",
+                "QUINTERO GARCIA JORGE ALEJANDRO",
+                "SANDOVAL HERNANDEZ LUZ MERY"
+            ],
+            8: [
+                "MALDONADO ROMO JIMENA",
+                "CANIZALEZ ESCUDERO JENNIFER",
+                "GOMEZ RUBIO ANDREA",
+                "PEREZ LOPEZ ALMA CRISTINA",
+                "SOLIS ARIAS LAURA ITZEL",
+                "LUNA PINEDA VICTOR MANUEL",
+                "GUZMAN ESTRADA ALBERTO",
+                "JIMENEZ PICAZO ALBERTO",
+                "OCAMPO GUTIERREZ LIZBETH",
+                "SALAS FLORES MAGDA",
+                "JUAREZ ARREZ ALEJANDRO"
+            ],
+            9:[
+                "PICHARDO ORDOÑEZ MIGUEL ANGEL",
+                "ESPITIA ALBORES CARLOS FERNANDO",
+                "GARCIA UBALDO GUADALUPE",
+                "HERNANDEZ GONZALEZ TANIA",
+                "RAMIREZ MORALES ELISUA SARAI",
+                "HERNANDEZ ALONSO AURORA",
+                "VILLALOBOS MATEOS LUCIO",
+                "RENDON TORRES ANDREA CECILIA",
+                "RUIZ JIMENEZ BRISA NORELY",
+                "ESPINOSA GARCIA MARIA GUADALUPE",
+                "ZAMORA SALAZAR MARIO",
+                "SANDOVAL MARTINEZ ROLANDO EMMANUEL"
+            ],
+            10:[
+                "SERRANO BELLO CARLOS ALBERTO",
+                "ESTRADA GARCIA MARY CARMEN",
+                "CRUZ LEON MA. GUADALUPE",
+                "RAYA RIVERA ATLANTIDA MARGARITA",
+                "MALDONADO BERNAL MARIA DEL CARMEN",
+                "GALICIA AMARO ULISES",
+                "ALVA ARANZA MARIA DE LOURDES",
+                "RUIZ HERNANDEZ MARISELA",
+            ],
+            11:[
+                "VILLALPANDO CARRION SALVADOR",
+                "GARCIA REGIL PATRICIA",
+                "GONZALEZ ABURTO ARACELI",
+                "SANCHEZ LUNA ESMERALDA ELIZABETH",
+                "CARRILLO GOMEZ MARIA CONCEPCION",
+                "CARRASCO ZENDEJAS IVONE",
+                "GUEDEA OCHOA RAFAEL ALEJANDRO",
+                "GARZA GALLEGOS KARLA PAOLA",
+                "JERONIMO DUARTE LUZ LETICIA",
+                "RODRIGUEZ ESPINOZA ERIKA"
+            ],
+            12:[
+                "JIMENEZ LOPEZ NELLY",
+                "LOPEZ CHAVEZ MIREYA",
+                "SANCHEZ MIGUEL MARIA ELENA",
+                "LOPEZ CORTEZ CARLOS ANTONIO",
+                "SANTANA ROMAN MONTSERRAT ISABEL",
+                "JIMENEZ FLORES GABRIELA LIZBETH",
+                "GONZALEZ GARCIA NADIA",
+                "RECINOS MORENO MARIA GUADALUPE",
+            ],
+            13:[
+                "ALAMILLO HERNANDEZ ALFREDO TRINIDAD",
+                "DULIZ DELGADO JESUS EDUARDO",
+                "MEDINA BOBADILLA BALERIA LILIANA",
+                "REYES PEDREZUELA REBECA",
+                "SANDOVAL MATA MIRIAM ANGELICA",
+                "VERA RICO SERGIO ARTURO",
+                "GUERRA ELVIRA ABIGAIL ARIADNA",
+                "VITE LOPEZ EDUARDO",
+            ],
         }
         // Agrega más cumpleaños según sea necesario
     };
